360fb9fe789f0e04b7f174b57f99003674277ec8
Gage Christensen <grchr4000@gmail.com>
2021-03-02 16:44:03 -0800

    Add HomePageTest

--- lists/tests.py
+++ lists/tests.py
@@ -2,8 +2,17 @@
 # Create your tests here.
 
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+
 
 class SmokeTest(TestCase):
 
     def test_math(self):
         self.assertEqual(8*8, 34)
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
